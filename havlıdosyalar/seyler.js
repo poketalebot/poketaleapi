e13989472916f7e90053b4f465e7bfc49c6d9608
Ash <65588168+ashpotter@users.noreply.github.com>
2021-01-26 10:44:52 +0300

    oh yeah

---
+++ havlıdosyalar/seyler.js
@@ -0,0 +1,12 @@
+// wow cool
+
+var domains = {
+  "Api": 'https://poketalebot.com/api',
+  "URL_WSS": 'wss://poketaleapi.tk',
+  "URL_HTTPS": 'https://poketaleapi.tk',
+  "DOMAİN": 'https://poketalebot.com',
+  "discord_gateway": 'wss://gateway.discord.gg/',
+  "discord_url": 'https://discord.com',
+}
+
+module.exports = domains
